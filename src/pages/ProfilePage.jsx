1b0c2ee35a91c6f3e78dcabda487a9954744b882
geo-web-maker <geo_web@yahoo.com>
2026-05-06 11:21:36 +0300

    Fix: Add missing useEffect import to ProfilePage.jsx

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,5 +1,5 @@
 // src/pages/ProfilePage.jsx
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 import useAppStore from '../store/useAppStore'
 import { logout } from '../hooks/useAuth'
